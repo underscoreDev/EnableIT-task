cb69ce8a1081ca8e2b3a8691b66ed23f329db117
underscoreDev <godswill@zimmerman.team>
2022-09-01 22:56:31 +0100

    feat: implement prev and next buttons

--- src/app/pages/landing-page/components/table/index.tsx
+++ src/app/pages/landing-page/components/table/index.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import { UserTableProps } from "app/interface/UserTable";
 
+const randomColors = [
+  "#00BFD9",
+  "#FFB834",
+  "#d90057",
+  "#9B51E0",
+  "#F2C94C",
+  " #27AE60",
+  "#000000",
+  "#2F80ED",
+  "#b5d900",
+  "#ce00d9",
+];
+
 export interface TableProps {
   users: UserTableProps[];
 }
 
-const Table = ({ users }: TableProps) => {
+const TableBody = ({ users }: TableProps) => {
   return (
     <>
-      {users.slice(0, 10).map((user: UserTableProps) => (
-        <tr className="table__row" key={user.ID}>
-          <td className="table__row--body">{user.FirstNameLastName}</td>
-          <td className="table__row--body">{user.Company}</td>
-          {/* <td className="table__row--body">{user.Email}</td> */}
-          <td className="table__row--body">{user.JobTitle}</td>
-          <td className="table__row--body">{user.Phone}</td>
-        </tr>
-      ))}
+      {users.slice(0, 10).map((user: UserTableProps, idx) => {
+        const firstNameInitial = user.FirstNameLastName.split(" ")[0].slice(0, 1);
+        const lastNameInitial = user.FirstNameLastName.split(" ")[1].slice(0, 1);
+        return (
+          <tr className="table__row" key={user.ID}>
+            <td className="table__row--body">
+              <span className="initials" style={{ background: `${randomColors[idx]}` }}>
+                {firstNameInitial}
+                {lastNameInitial}
+              </span>{" "}
+              {user.FirstNameLastName}
+            </td>
+            <td className="table__row--body">{user.Company}</td>
+            <td className="table__row--body">{user.JobTitle}</td>
+            <td className="table__row--body">{user.Phone}</td>
+          </tr>
+        );
+      })}
     </>
   );
 };
 
-export default Table;
+export default TableBody;
--- src/app/pages/landing-page/layout.tsx
+++ src/app/pages/landing-page/layout.tsx
@@ -1,7 +1,7 @@
 import Users from "app/utils/api";
 import { useQuery } from "react-query";
 import React, { useState } from "react";
-import Table from "app/pages/landing-page/components/table";
+import TableBody from "app/pages/landing-page/components/table";
 import CircularProgress from "@mui/material/CircularProgress";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -17,16 +17,17 @@ export const LandingLayout = () => {
   return (
     <React.Fragment>
       <section className="landing">
+        <img src="https://enableit.dk/images/logo.png" alt="EnableIT logo" />
+
         <div className="tableContainer">
           {isError ? (
             <p>Error Can't get Users</p>
           ) : (
-            <div>
+            <div className="main">
               <table className="table" aria-label="users table">
                 <tr className="table__row">
                   <th className="table__row--header">User Name</th>
                   <th className="table__row--header">Company</th>
-                  {/* <th className="table__row--header">Email</th> */}
                   <th className="table__row--header">Job Title</th>
                   <th className="table__row--header">
                     <PhoneEnabledIcon /> Phone Number
@@ -36,22 +37,23 @@ export const LandingLayout = () => {
                 {isLoading || isFetching ? (
                   <CircularProgress sx={{ color: "#ffdaa1" }} />
                 ) : (
-                  <Table users={data?.data.users} />
+                  <TableBody users={data?.data.users} />
                 )}
               </table>
-
-              {/* <button onClick={() => setPage(page - 1)} disabled={page === 0}>
-                <ArrowBackIosNewIcon />
-                prev
-              </button>
-
-              <button onClick={() => setPage(page + 1)}>
-                <ArrowForwardIosIcon />
-                next
-              </button> */}
             </div>
           )}
         </div>
+
+        <section className="btn">
+          <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+            <ArrowBackIosNewIcon />
+            Prev
+          </button>
+
+          <button onClick={() => setPage(page + 1)}>
+            Next <ArrowForwardIosIcon />
+          </button>
+        </section>
       </section>
     </React.Fragment>
   );
